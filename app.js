b0bbe6d66738272d9cbfe21b4dee3fd21b967e14
NataliSabrina <nescobartrujillo@gmail.com>
2025-03-16 19:07:46 -0500

    final

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const listAmigos = {};
+const inputaAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const uLListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    listaAmigos.push(inputaAmigo.value);
+   // uLListaAmigos.innerHTML = uLListaAmigos.innerHTML + inputaAmigo.value es igual a lo siguiente:
+   uLListaAmigos.innerHTML += `<li>${inputaAmigo.value}</li>` ;
+}
+
+function sortearAmigo() {
+    const random = Math.floor(Math.random() * listaAmigos.length);
+   const amigoSecreto = listaAmigos[random];
+   ulResultado.innerHTML = `<li> El amigo secreto es:  ${amigoSecreto}</li>`;
+}
