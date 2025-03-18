8016a4628304542688440ea3034c13e752e50a49
NataliSabrina <nescobartrujillo@gmail.com>
2025-03-17 21:09:33 -0500

    modicacion2

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const inputaAmigo = document.getElementById("amigo");
 const listaAmigos = [];
 const uLListaAmigos = document.getElementById("listaAmigos");
 const ulResultado = document.getElementById("resultado");
-
+ 
 function agregarAmigo() {
     listaAmigos.push(inputaAmigo.value);
    // uLListaAmigos.innerHTML = uLListaAmigos.innerHTML + inputaAmigo.value es igual a lo siguiente:
    uLListaAmigos.innerHTML += `<li>${inputaAmigo.value}</li>` ;
+   return;
 }
 
 function sortearAmigo() {
     const random = Math.floor(Math.random() * listaAmigos.length);
    const amigoSecreto = listaAmigos[random];
    ulResultado.innerHTML = `<li> El amigo secreto es:  ${amigoSecreto}</li>`;
+  
+      return;
 }
